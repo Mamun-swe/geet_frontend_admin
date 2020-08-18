0bf6c7c58be963583ca4f2d04e24e4be83a8ac6f
Abdullah Al Mamun <mhmamun166009@gmail.com>
2020-08-18 16:10:32 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import "bootstrap/scss/bootstrap.scss"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="card">
+                <div className="card-body">
+                  <h5>Hello react sass</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
